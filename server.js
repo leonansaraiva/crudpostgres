cb6e9a2944a97ba358366e9eac33619c56c687f2
le053425 <le053425@intelbras.com.br>
2022-03-28 11:24:41 -0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
 
 require('dotenv').config();
 
